3212f469a1f12f71ae7cc5cf9972181da766fc8a
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-01-13 11:19:06 +0100

    indention et al

--- src/mongo/core.clj
+++ src/mongo/core.clj
@@ -13,14 +13,12 @@
   (let [conn (mg/connect)
         db   (mg/get-db conn "bemyeyes")
         yesterday (t/minus (t/now) (t/days 1))]
-
     (with-collection db "event_logs"
                      (find {:name event_name, :created_at { $gt yesterday}})
                      (fields [ :created_at :name ])
                      ;; note the use of sorted maps with sort
                      (sort (sorted-map :created_at -1))
-                     (limit max-limit)
-                     )))
+                     (limit max-limit))))
 
 (defn all-helper-notified
   ([] (all-helper-notified 10))
